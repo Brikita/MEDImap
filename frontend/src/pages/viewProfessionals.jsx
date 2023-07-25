4b520bf460a2b344c35ebde55a23356269d83303
Brian <134489011+Brikita@users.noreply.github.com>
2023-07-25 22:33:37 +0300

    create ui files

---
+++ frontend/src/pages/viewProfessionals.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const viewProfessionals = () => {
+  return (
+    <div>view Professionals</div>
+  )
+}
+
+export default viewProfessionals
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
  const connectDB = require('./DB/connectDB');
 const blogRoutes = require('./Routes/blogsRoutes');
@@ -7,6 +8,7 @@ const userRoutes = require('./Routes/usersRoutes');
 connectDB();
  // Middleware
 app.use(express.json());
+app.use(cors())
  // Routes
 app.use('/api/blogs', blogRoutes);
 app.use('/api/users', userRoutes);
