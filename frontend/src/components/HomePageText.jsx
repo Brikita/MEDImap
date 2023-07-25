b9f9294f69a92f19e772a8fc1d17b7b2910236a7
Brian <134489011+Brikita@users.noreply.github.com>
2023-07-25 22:32:22 +0300

    create homepage text

---
+++ frontend/src/components/HomePageText.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomePageText = () => {
+  return (
+    <div className='homepageContent'>
+        <h1>Mental Health Awareness</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur animi doloribus omnis dolores aut, illum blanditiis quibusdam ab culpa minus doloremque voluptas eligendi rem quas vero voluptatum sed laudantium. Quaerat!</p>
+    </div>
+  )
+}
+
+export default HomePageText
