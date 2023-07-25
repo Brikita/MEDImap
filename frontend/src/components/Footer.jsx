3f0ee31fe6b31d6da057a2e17ffff15b7ae296c3
Brian <134489011+Brikita@users.noreply.github.com>
2023-07-25 22:31:59 +0300

    create  footer component

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Footer.css';
+ const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__content">
+        <p className="footer__text">© 2023 MEDImap All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
+ export default Footer;
