968fac6ed46ac190190de6ba5ecedcbff73baea3
Brian <134489011+Brikita@users.noreply.github.com>
2023-07-26 21:09:19 +0300

    add routes to the profile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Profile from './pages/profile.jsx'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import Home from './pages/home'
+import Home from './pages/dash/Home'
+import ReadBlog from './pages/dash/ReadBlog.jsx'
+import CreateBlog from './pages/dash/CreateBlog.jsx'
 
 const App = () => {
   return (
     <div>
       <Header />
-      <Home />
+      <Routes>
+       <Route index element={<Home />} />
+       <Route path='/blogs/:id' element={<ReadBlog />} />
+        <Route path='/profile' >
+          <Route index element={<Profile />}/>
+
+          <Route path='new' element={<CreateBlog />} />
+        </Route>
+
+      </Routes>
       <Footer />
     </div>
   )
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,18 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
-import Profile from './pages/profile.jsx'
-import Login from './pages/login.jsx'
+import { store } from './app/store.jsx'
+import { Provider } from 'react-redux'
+import Home from './pages/Home.jsx'
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/' element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/dash/*' element={<App />} />
+         <Route path='/' element={<Home />} />
+
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 )
