d36b3198be1f0cf49679cf0d18ec2ad272aa8368
Brian <134489011+Brikita@users.noreply.github.com>
2023-07-26 21:05:00 +0300

    create a get professionals query

---
+++ frontend/src/features/user/userApiSlice.jsx
@@ -0,0 +1,120 @@
+import { createSelector, createEntityAdapter } from "@reduxjs/toolkit";
+import { apiSlice } from "../../app/api/ApiSlice";
+
+const usersAdapter = createEntityAdapter({})
+
+const initialState = usersAdapter.getInitialState()
+
+export const usersApiSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+      getUsers: builder.query({
+        query: () => '/users/professionals',
+        validateStatus: (response, result) => {
+          return response.status === 200 && !result.isError;
+        },
+        transformResponse: (responseData) => {
+          const loadedUsers = responseData.map((user) => {
+            user.id = user._id;
+            return user;
+          });
+          return usersAdapter.setAll(initialState, loadedUsers);
+        },
+        providesTags: (result, error, arg) => {
+          if (result.ids) {
+            return [
+              { type: 'User', id: 'LIST' },
+              ...result.ids.map((id) => ({ type: 'User', id })),
+            ];
+          } else return [{ type: 'User', id: 'LIST' }];
+        },
+      }),
+      createUser: builder.mutation({
+        query: (user) => ({
+          url: '/users',
+          method: 'POST',
+          body: user,
+        }),
+        validateStatus: (response, result) => {
+          return response.status === 201 && !result.isError;
+        },
+        transformResponse: (responseData) => {
+          const createdUser = {
+            id: responseData._id,
+            ...responseData,
+          };
+          return usersAdapter.addOne(initialState, createdUser);
+        },
+        invalidatesTags: [{ type: 'User', id: 'LIST' }],
+      }),
+    }),
+  });
+
+export const {
+    useGetUsersQuery,
+    useCreateUserMutation,
+ } = usersApiSlice
+
+export const selectUsersResult = usersApiSlice.endpoints.getUsers.select()
+
+const selectUsersData = createSelector(
+    selectUsersResult,
+    usersResult => usersResult.data
+)
+
+export const {
+    selectAll: selectAllUsers,
+    selectById: selectUsersById,
+    selectIds: selectUserIds,
+} = usersAdapter.getSelectors(state => selectUsersData(state) ?? initialState)
+
+
+
+
+
+/* import { createAsyncThunk, createSlice, createEntityAdapter } from "@reduxjs/toolkit";
+import { apiSlice } from "../../app/api/ApiSlice";
+
+
+const usersAdapter = createEntityAdapter({})
+const initialState = usersAdapter.getInitialState()
+
+export const getUsers = createAsyncThunk(
+    "users/getUsers",
+    async () => {
+        const response = await fetch("/users/professionals");
+        if (!response.ok) {
+            throw new Error("Failed to fetch professionals");
+        }
+        const data = await response.json();
+        return data.map((user) => {
+            user.id = user._id;
+            return user;
+        });
+    }
+);
+export const usersSlice = createSlice({
+    name: "users",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(getUsers.fulfilled, (state, action) => {
+                usersAdapter.setAll(state, action.payload);
+            });
+    },
+});
+export const { selectAll: selectAllUsers, selectById: selectUserById } = usersAdapter.getSelectors((state) => state.users);
+export const { useGetUsersQuery } = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        getUsers: builder.query({
+            query: () => "/users/professionals",
+            transformResponse: (responseData) => {
+                return responseData.map((user) => {
+                    user.id = user._id;
+                    return user;
+                });
+            },
+        }),
+    }),
+});
+export default usersSlice.reducer; */
