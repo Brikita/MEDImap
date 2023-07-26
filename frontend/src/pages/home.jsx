12e8e4e23e3e0f3246dc3a9526beffec56985f0a
Brian <134489011+Brikita@users.noreply.github.com>
2023-07-26 21:08:53 +0300

    set up unprotected page

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import SignupForm from '../components/SignUpForm'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 import HomePageText from '../components/HomePageText'
 import './home.css'
 
 const Home = () => {
   return (
-    <div className='home-page'>
-      <HomePageText />
-      <SignupForm />
-    </div>
+    <>
+      <Header />
+
+      <div className='home-page'>
+        <HomePageText />
+        <SignupForm />
+      </div>
+      <Footer />
+    </>
   )
 }
 
