6c0e51896f0a0f7af741eac8c2c5a3d460212ecf
Brian <134489011+Brikita@users.noreply.github.com>
2023-07-29 08:05:40 +0300

    add the about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,55 +1,69 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import http from '../lib/http'
-
+import Spinner from '../components/Spinner'
 const About = () => {
   const { handleSubmit, register } = useForm()
+  const [isSubmitted, setIsSubmitted] = useState('')
+  const [isLoading, setIsLoading] = useState('')
 
 
   const onSubmit = async ({ name, email, content }) => {
-    const payload = { name, email, content }
-
-    const res = await http.post('/api/feedback', {data: payload})
-
-
-
-  }
-
+    setIsSubmitted(false)
+    setIsLoading(true)
+    const payload = { name, email, content };
+    try {
+      const res = await http.post('/api/feedback', { data: payload });
+      const response = res.data;
+      setIsSubmitted(true)
+      setIsLoading(false)
+      alert('Feedback submitted successfully!');
+      console.log(response);
+    } catch (err) {
+      console.error(err);
+      alert('Error submitting feedback. Please try again later.');
+    }
+  };
 
   return (
     <div>
       <Header />
       <h1>MEDImap</h1>
-      <section classNameName='feedback-section'>
+      <section className='feedback-section'>
         <p>Welcome to MEDImap, a compassionate platform dedicated to empowering mental health and fostering overall well-being. We provide a safe and supportive space with resources, a community, helplines, and therapeutic tools for individuals facing mental health challenges. MEDImap follows a user-centered approach, offering personalized support and expert-curated information. Join us on this transformative journey towards embracing mental well-being and unlocking the strength within. Let's MEDImap together.
         </p>
       </section>
-      <section className='feedback-form'>
+      {!isSubmitted ? <section className='feedback-form'>
         <h2>Submit Feedback</h2>
         <h4>Your Feedback is valuable to us. Please consider leaving a message</h4>
         <div className="card-feedback">
           <span className="title">Leave a Comment</span>
           <form className="form-feedback" onSubmit={handleSubmit(onSubmit)}>
             <div className="group">
-              <input placeholder="‎" type="text" required="" {...register('name')} />
-              <label for="name">Name</label>
+              <input placeholder="‎" type="text" required {...register('name')} />
+              <label htmlFor="name">Name</label>
             </div>
             <div className="group">
-              <input placeholder="‎" type="email" id="email" name="email" required="" {...register('email')} />
-              <label for="email">Email</label>
+              <input placeholder="‎" type="email" id="email" name="email" required {...register('email')} />
+              <label htmlFor="email">Email</label>
             </div>
             <div className="group">
-              <textarea placeholder="‎" id="comment" name="comment" rows="5" required="" {...register('content')}></textarea>
-              <label for="comment">Comment</label>
+              <textarea placeholder="‎" id="comment" name="comment" rows="5" required {...register('content')}></textarea>
+              <label htmlFor="comment">Comment</label>
             </div>
             <button type="submit">Submit</button>
+            {isLoading && <div><Spinner /> <p>Submitting Please wait...</p></div>}
           </form>
         </div>
 
-      </section>
+      </section> :
+        <div className='card-feedback-submit'>
+          <p>Submitted. Thanks for the feedback ;&gt;)</p>
+        </div>
+      }
       <section>
         <Link to='/' >Back to Login</Link>
       </section>
